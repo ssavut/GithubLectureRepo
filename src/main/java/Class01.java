0553761e4a91d0454cf82dedc3edf8e72b444a83
Sümeyye Savut <sy.temuh@gmail.com>
2023-08-19 21:15:34 +0300

    v1

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -1,17 +1,8 @@
 public class Class01 {
     public static void main(String[] args) {
 
-        //Versiyon1
-        int x = 1;
-        int y = 2;
-        System.out.println(x + y);
+        //Versiyon1 Kodları
+        System.out.println("Versiyon1");
 
-
-        //Versiyon 2
-        int z = 5;
-        System.out.println(x + y + z);
-
-        //Versiyon 3
-        System.out.println("Versiyon 3 için kodlar");
     }
 }
