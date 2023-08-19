db47777179358e7a46499527661243cccf9e4ac0
Sümeyye Savut <sy.temuh@gmail.com>
2023-08-19 20:39:16 +0300

    Versiyon 3

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -10,5 +10,8 @@ public class Class01 {
         //Versiyon 2
         int z = 5;
         System.out.println(x + y + z);
+
+        //Versiyon 3
+        System.out.println("Versiyon 3 için kodlar");
     }
 }
